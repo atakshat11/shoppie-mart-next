fcaae5835d082ff1cf718b488706a0a1dfee9397
Akshat Mehta <akshatm.inexture@gmail.com>
2022-07-26 17:11:26 +0530

    Shoppie-Mart Next Updated.

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import ProductList from "../components/ProductList";
+import { Container, Row } from "react-bootstrap";
+const Home = ({ products }) => {
+  return (
+    <>
+      <Container className="my-3 p-3">
+        <Row md={4} className="justify-content-start disp-product">
+          {products.map((product, index) => (
+            <ProductList product={product} key={index} />
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+export async function getServerSideProps() {
+  const res = await fetch(`/api/products`);
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data,
+    },
+  };
+}
+export default Home;
